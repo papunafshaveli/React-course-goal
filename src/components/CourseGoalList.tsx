cdfa301f9682a1cc3892efc87f4e9a0b767dbb50
Papuna Fshaveli <papunafshaveli@gmail.com>
2023-11-21 10:09:57 +0400

    add Infobox component

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,21 +1,38 @@
 import CourseGoal from "./CourseGoal";
 import { CourseGoalProps } from "../App";
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 type CourseGoalListProps = {
   goals: CourseGoalProps[];
   onDeleteGoal: (id: number) => void;
 };
 
 const CourseGoalList = ({ goals, onDeleteGoal }: CourseGoalListProps) => {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">Start adding course goals</InfoBox>;
+  }
+
+  let warningBox: ReactNode;
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode="warning">You are collecting too many goals!</InfoBox>
+    );
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            <p>{goal.description}</p>
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
